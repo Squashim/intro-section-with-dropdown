95a3bebb7bdc5d585908f63f6bc19b0bd5c1f41c
Squashim <konrad18v1@wp.pl>
2022-07-24 11:21:12 +0200

    86% done

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const menuOpen = document.querySelector(".menu-btn");
 const menuClose = document.querySelector(".menu-close");
 const menuContainer = document.querySelector(".menu-container");
+const pageMask = document.querySelector(".page-mask");
 
 menuOpen.addEventListener("click", () => {
     menuContainer.style.display = "flex";
+    pageMask.style.display = "block";
 })
 
 menuClose.addEventListener("click", () => {
     menuContainer.style.display = "none";
+    pageMask.style.display = "none";
+
 })
