5c7f15c00ab9d124867d1a55eaff92d96dd4b9c0
Squashim <konrad18v1@wp.pl>
2022-07-24 12:52:35 +0200

    90% done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menuOpen = document.querySelector(".menu-btn");
 const menuClose = document.querySelector(".menu-close");
 const menuContainer = document.querySelector(".menu-container");
 const pageMask = document.querySelector(".page-mask");
+const menuFeatures = document.querySelector(".features")
+const menuCompany = document.querySelector(".company");
 
 menuOpen.addEventListener("click", () => {
     menuContainer.style.display = "flex";
@@ -13,3 +15,15 @@ menuClose.addEventListener("click", () => {
     pageMask.style.display = "none";
 
 })
+
+function openMenuList(list){
+    if(list.parentElement.children[1].style.display == "none"){
+        list.children[0].src = "./images/icon-arrow-up.svg";
+        list.parentElement.children[1].style.display = "flex";
+    } else {
+        list.children[0].src = "./images/icon-arrow-down.svg";
+        list.parentElement.children[1].style.display = "none";
+    }
+}
+menuFeatures.addEventListener("click", () => {openMenuList(menuFeatures)})
+menuCompany.addEventListener("click", () => {openMenuList(menuCompany)})
